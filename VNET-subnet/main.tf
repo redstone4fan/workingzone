3dc0bb19b1372eff60c5656dae056ea3f7abeeac
redstone4fan <redstone0@gmail.com>
2021-12-30 19:24:31 -0500

    change

--- VNET-subnet/main.tf
+++ VNET-subnet/main.tf
@@ -37,6 +37,6 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "sn" {
    name                 =   var.subnet_name
    resource_group_name  =   data.azurerm_resource_group.rg.name
-   virtual_network_name =   data.azurerm_virtual_network.vnet.name
+   virtual_network_name =   azurerm_virtual_network.vnet.name
    address_prefixes     =   [var.subnet_address_range]
 }
